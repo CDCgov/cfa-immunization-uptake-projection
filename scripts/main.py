f6c1e6c154e984945cd2ed28b279c046c8d9bf42
Scott Olesen <ulp7@cdc.gov>
2024-11-01 12:44:08 -0400

    use argparse

--- scripts/main.py
+++ scripts/main.py
@@ -1,40 +1,48 @@
 import yaml
 import iup
+import argparse
 
-# Access the YAML config file
-with open(
-    "/home/tec0/cfa-immunization-uptake-projection/config/config.yaml", "r"
-) as file:
-    config = yaml.safe_load(file)
 
-# List of the cumulative data sets described in the yaml
-cumulative_data = [iup.parse_nis(**x) for x in config["data"].values()]
+def run(config):
+    # List of the cumulative data sets described in the yaml
+    cumulative_data = [iup.parse_nis(**x) for x in config["data"].values()]
 
-# List of incident data sets from the cumulative data sets
-incident_data = [x.to_incident() for x in cumulative_data]
+    # List of incident data sets from the cumulative data sets
+    incident_data = [x.to_incident() for x in cumulative_data]
 
-# Concatenate data sets and split into train and test subsets
-cumulative_train_data = iup.CumulativeUptakeData.split_train_test(
-    cumulative_data, config["timeframe"]["start"], "train"
-)
-cumulative_test_data = iup.CumulativeUptakeData.split_train_test(
-    cumulative_data, config["timeframe"]["start"], "test"
-)
-incident_train_data = iup.IncidentUptakeData.split_train_test(
-    incident_data, config["timeframe"]["start"], "train"
-)
-incident_test_data = iup.IncidentUptakeData.split_train_test(
-    incident_data, config["timeframe"]["start"], "test"
-)
-
-# Fit models using the training data and make projections
-incident_model = (
-    iup.LinearIncidentUptakeModel()
-    .fit(incident_train_data)
-    .predict(
-        config["timeframe"]["start"],
-        config["timeframe"]["end"],
-        config["timeframe"]["interval"],
+    # Concatenate data sets and split into train and test subsets
+    cumulative_train_data = iup.CumulativeUptakeData.split_train_test(
+        cumulative_data, config["timeframe"]["start"], "train"
     )
-)
-print(incident_model.cumulative_projection)
+    cumulative_test_data = iup.CumulativeUptakeData.split_train_test(
+        cumulative_data, config["timeframe"]["start"], "test"
+    )
+    incident_train_data = iup.IncidentUptakeData.split_train_test(
+        incident_data, config["timeframe"]["start"], "train"
+    )
+    incident_test_data = iup.IncidentUptakeData.split_train_test(
+        incident_data, config["timeframe"]["start"], "test"
+    )
+
+    # Fit models using the training data and make projections
+    incident_model = (
+        iup.LinearIncidentUptakeModel()
+        .fit(incident_train_data)
+        .predict(
+            config["timeframe"]["start"],
+            config["timeframe"]["end"],
+            config["timeframe"]["interval"],
+        )
+    )
+    print(incident_model.cumulative_projection)
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("--config", type=str, default="config/config.yaml")
+    args = p.add_argument()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
+    run(config)
