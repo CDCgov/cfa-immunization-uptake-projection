ec83cb225e3d64248670bfcda507fb7e95b36111
Edward Schrom <tec0@cdc.gov>
2024-11-08 14:12:52 -0500

    Generalized main.py for arbitrary grouping factors

--- scripts/main.py
+++ scripts/main.py
@@ -7,31 +7,30 @@ def run(config):
     # List of the cumulative data sets described in the yaml
     cumulative_data = [iup.parse_nis(**x) for x in config["data"].values()]
 
+    # List of grouping factors used in each data set
+    grouping_factors = iup.extract_group_names(
+        [x["group_cols"] for x in config["data"].values()]
+    )
+
     # List of incident data sets from the cumulative data sets
-    incident_data = [x.to_incident() for x in cumulative_data]
+    incident_data = [
+        x.to_incident(y) for x, y in zip(cumulative_data, grouping_factors)
+    ]
 
     # Concatenate data sets and split into train and test subsets
-    # cumulative_train_data = iup.CumulativeUptakeData.split_train_test(
-    #    cumulative_data, config["timeframe"]["start"], "train"
-    # )
-    # cumulative_test_data = iup.CumulativeUptakeData.split_train_test(
-    #    cumulative_data, config["timeframe"]["start"], "test"
-    # )
     incident_train_data = iup.IncidentUptakeData.split_train_test(
         incident_data, config["timeframe"]["start"], "train"
     )
-    # incident_test_data = iup.IncidentUptakeData.split_train_test(
-    #    incident_data, config["timeframe"]["start"], "test"
-    # )
 
     # Fit models using the training data and make projections
     incident_model = (
         iup.LinearIncidentUptakeModel()
-        .fit(incident_train_data)
+        .fit(incident_train_data, grouping_factors[0])
         .predict(
             config["timeframe"]["start"],
             config["timeframe"]["end"],
             config["timeframe"]["interval"],
+            grouping_factors[0],
         )
     )
     print(incident_model.cumulative_projection)
