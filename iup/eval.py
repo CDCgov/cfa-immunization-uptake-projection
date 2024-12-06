56e1a2978679561e66301d5df796a936ad5715c0
Fuhan Yang <ab32@cdc.gov>
2024-12-06 12:15:15 -0500

    update test_eval and eval module

--- iup/eval.py
+++ iup/eval.py
@@ -1,117 +1,96 @@
-from iup import IncidentUptakeData, PointForecast
 import polars as pl
+from iup import IncidentUptakeData, PointForecast
+from typing import Callable
 
 
-def get_mspe(data: IncidentUptakeData, pred: PointForecast) -> pl.DataFrame:
+###### evaluation metrics #####
+def check_date_match(data: IncidentUptakeData, pred: PointForecast):
     """
-    Calculate MSPE between data and pred
+    Check the dates between data and pred.
+    Dates must be 1-on-1 equal and no duplicate.
     ----------------------
-    Input: data, pred in a matching and validated format
-    Return: pl.DataFrame with MSPE and the forecast start date and end date
+
+    Parameters
+    data:
+        The observed data used for modeling. Should be IncidentUptakeData
+    pred:
+        The forecast made by model. Should be PointForecast
+
+    Return
+    Error if conditions fail to meet.
 
     """
-    # Check the conditions for date match:
-    # 1. Mutual dates must exist between data and prediction.
-    assert any(
-        data["date"].is_in(pred["date"])
-    ), "No matched dates between data and prediction."
+    # sort data and pred by date #
+    data = data.sort("date")
+    pred = pred.sort("date")
+
+    # 1. Dates must be 1-on-1 equal
+    (data["date"] == pred["date"]).all()
 
     # 2. There should not be any duplicated date in either data or prediction.
-    common_dates = data.filter(pl.col("date").is_in(pred["date"])).select("date")
-
     assert (
-        len(common_dates) == common_dates.n_unique()
-    ), "Duplicated dates are found in data or prediction."
-
-    joined = data.join(pred, on="date", how="inner", validate="1:1")
-
-    start = (
-        joined.filter(
-            pl.col("date") == pl.col("date").min(),
-        )
-        .rename({"date": "forecast_start"})
-        .select("forecast_start")
-    )
-
-    end = (
-        joined.rename({"estimate": "data", "estimate_right": "pred"})
-        .with_columns(spe=(pl.col("data") - pl.col("pred")) ** 2)
-        .with_columns(
-            mspe=pl.col("spe").mean(),
-        )
-        .filter(pl.col("date") == pl.col("date").max())
-        .rename({"date": "forecast_end"})
-        .select("forecast_end", "mspe")
-    )
-
-    return pl.concat([start, end], how="horizontal")
+        len(data["date"]) == data["date"].n_unique()
+    ), "Duplicated dates are found in data and prediction."
 
 
-def get_mean_bias(data: IncidentUptakeData, pred: PointForecast) -> pl.DataFrame:
+def score(
+    data: IncidentUptakeData,
+    pred: PointForecast,
+    score_fun: Callable[[pl.Expr, pl.Expr], pl.Expr],
+) -> pl.DataFrame:
     """
-    Calculate Mean bias from joined data.
-    Note the bias here is not the classical bias calculated from the posterior distribution.
+    Calculate score between observed data and forecast.
+    ----------------------
 
+    Parameters
+    data:
+        The observed data used for modeling. Should be IncidentUptakeData
+    pred:
+        The forecast made by model. Should be PointForecast
+    score_fun:
+        Scoring function. Takes observed and true values.
+
+    Return
+    pl.DataFrame with one row: forecast start date, forecast end date, and score
+
+    """
+    # validate inputs
+    data = IncidentUptakeData(data)
+    pred = PointForecast(pred)
+    check_date_match(data, pred)
+
+    return (
+        data.join(pred, on="date", how="inner", validate="1:1")
+        .rename({"estimate": "data", "estimate_right": "pred"})
+        .select(
+            forecast_start=pl.col("date").min(),
+            forecast_end=pl.col("date").max(),
+            score=score_fun(pl.col("data"), pl.col("pred")),
+        )
+    )
+
+
+def mspe(x: pl.Expr, y: pl.Expr) -> pl.Expr:
+    return ((x - y) ** 2).mean()
+
+
+def mean_bias(pred: pl.Expr, data: pl.Expr) -> pl.Expr:
+    """
+    Note the bias here is not the classical bias calculated from the posterior distribution.
     The bias here is defined as: at time t,
     bias = -1 if pred_t < data_t; bias = 0 if pred_t == data_t; bias = 1 if pred_t > bias_t
 
     mean_bias = sum of the bias across time/length of data
-    -------------------------
-    Input: data, pred in a matching and validated format
-    Return: pl.DataFrame with mean bias and the forecast start date and end date
     """
 
-    # Check the conditions for date match:
-    # 1. Mutual dates must exist between data and prediction.
-    assert any(
-        data["date"].is_in(pred["date"])
-    ), "No matched dates between data and prediction."
-
-    # 2. There should not be any duplicated date in either data or prediction.
-    common_dates = data.filter(pl.col("date").is_in(pred["date"])).select("date")
-
-    assert (
-        len(common_dates) == common_dates.n_unique()
-    ), "Duplicated dates are found in data or prediction."
-
-    joined = (
-        data.join(pred, on="date", how="inner", validate="1:1")
-        .rename({"estimate": "data", "estimate_right": "pred"})
-        .with_columns(diff=(pl.col("data") - pl.col("pred")))
-    )
-
-    joined = joined.with_columns(bias=joined["diff"].sign())
-
-    m_bias = pl.DataFrame(
-        {
-            "forecast_start": joined["date"].min(),
-            "forecast_end": joined["date"].max(),
-            "mbias": joined["bias"].sum() / joined.shape[0],
-        }
-    )
-
-    return m_bias
+    return (pred - data).sign().mean()
 
 
-def get_eos_abe(data: IncidentUptakeData, pred: PointForecast) -> pl.DataFrame:
+def eos_abe(data: pl.Expr, pred: pl.Expr) -> pl.Expr:
     """
-    Calculate the absolute error of the total uptake at the end of season between data and prediction.
-    -------------------
-    Input: data, pred in a matching and validated format
-    Return: pl.DataFrame with absolute error in the total uptake between data and prediction
-            and the forecast end date.
+    Calculate the absolute error of the total uptake at the end of season between data and prediction
+    relative to data.
     """
-    joined = (
-        data.join(pred, on="date", how="inner", validate="1:1")
-        .rename({"estimate": "data", "estimate_right": "pred"})
-        .with_columns(
-            cumu_data=pl.col("data").cum_sum(),
-            cumu_pred=pl.col("pred").cum_sum(),
-        )
-        .filter(pl.col("date") == pl.col("date").max())
-        .rename({"date": "forecast_end"})
-    )
-
-    abe_perc = abs(joined["cumu_data"] - joined["cumu_pred"]) / joined["cumu_data"]
-
-    return pl.DataFrame([joined["forecast_end"], abe_perc])
+    cum_data = data.cum_sum().tail(1)
+    cum_pred = pred.cum_sum().tail(1)
+    return abs(cum_data - cum_pred) / cum_data
