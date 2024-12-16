1a2a67289a8ba6342d3310b165c0d715406af26e
Edward Schrom <tec0@cdc.gov>
2024-12-16 12:06:58 -0500

    Updated tests to reflect grouping factor and date changes

--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -15,7 +15,7 @@ def frame():
     frame = pl.DataFrame(
         {
             "geography": ["USA", "PA", "USA"],
-            "date": ["2020-01-07", "2020-01-14", "2020-01-21"],
+            "time_end": ["2020-01-07", "2020-01-14", "2020-01-21"],
             "estimate": [0.0, 0.1, 0.2],
             "indicator": ["refused", "booster", "booster"],
         }
@@ -28,26 +28,34 @@ def test_insert_rollout_handles_groups(frame):
     """
     If grouping columns are given to insert_rollout, a separate rollout is inserted for each group.
     """
-    frame = frame.with_columns(date=pl.col("date").str.strptime(pl.Date, "%Y-%m-%d"))
+    frame = frame.with_columns(
+        date=pl.col("time_end").str.strptime(pl.Date, "%Y-%m-%d")
+    )
     rollout = [dt.date(2020, 1, 1), dt.date(2021, 1, 1)]
-    group_cols = ("geography",)
+    group_cols = [
+        "geography",
+    ]
     frame = iup.CumulativeUptakeData(frame.drop("indicator"))
 
     output = frame.insert_rollout(rollout, group_cols)
 
     assert output.shape[0] == 7
     assert (
-        output["date"].value_counts().filter(pl.col("date") == rollout[0])["count"][0]
+        output["time_end"]
+        .value_counts()
+        .filter(pl.col("time_end") == rollout[0])["count"][0]
         == 2
     )
-    assert output["date"].is_sorted()
+    assert output["time_end"].is_sorted()
 
 
 def test_insert_rollout_handles_no_groups(frame):
     """
     If no grouping columns are given to insert_rollout, only one of each rollout is inserted.
     """
-    frame = frame.with_columns(date=pl.col("date").str.strptime(pl.Date, "%Y-%m-%d"))
+    frame = frame.with_columns(
+        date=pl.col("time_end").str.strptime(pl.Date, "%Y-%m-%d")
+    )
     rollout = [dt.date(2020, 1, 1), dt.date(2021, 1, 1)]
     group_cols = None
     frame = iup.CumulativeUptakeData(frame.drop(["indicator", "geography"]))
@@ -56,53 +64,24 @@ def test_insert_rollout_handles_no_groups(frame):
 
     assert output.shape[0] == 5
     assert (
-        output["date"].value_counts().filter(pl.col("date") == rollout[0])["count"][0]
+        output["time_end"]
+        .value_counts()
+        .filter(pl.col("time_end") == rollout[0])["count"][0]
         == 1
     )
-    assert output["date"].is_sorted()
-
-
-def test_extract_group_names_handles_matched_groups():
-    """
-    If a list of identical group name lists is given, just one tuple is returned.
-    """
-    group_cols = [["geography", "indicator"], ["geography", "indicator"]]
-
-    output = iup.extract_group_names(group_cols)
-
-    assert output == ("geography", "indicator")
-
-
-def test_extract_group_names_handles_unmatched_groups():
-    """
-    If a list of non-identical group name lists is given, an error is raised.
-    """
-    group_cols = [["geography", "indicator"], ["geography", "mismatch"]]
-
-    with pytest.raises(AssertionError):
-        iup.extract_group_names(group_cols)
-
-
-def test_extract_group_names_handles_no_groups():
-    """
-    If a list group name lists containing None is given, an error is raised.
-    """
-    group_cols = [[None], ["geography", "mismatch"]]
-
-    with pytest.raises(AssertionError):
-        iup.extract_group_names(group_cols)
+    assert output["time_end"].is_sorted()
 
 
 def test_quantile_forecast_validation():
     with pytest.raises(AssertionError, match="quantile"):
         iup.QuantileForecast(
-            {"quantile": [-0.1], "date": [dt.date(2020, 1, 1)], "estimate": [0.0]}
+            {"quantile": [-0.1], "time_end": [dt.date(2020, 1, 1)], "estimate": [0.0]}
         )
 
 
 def test_sample_forecast_validation():
     iup.SampleForecast(
         pl.DataFrame(
-            {"date": [dt.date(2020, 1, 1)], "estimate": [0.0], "sample_id": 0}
+            {"time_end": [dt.date(2020, 1, 1)], "estimate": [0.0], "sample_id": 0}
         ).with_columns(pl.col("sample_id").cast(pl.Int64))
     )
