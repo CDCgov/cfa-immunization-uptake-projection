50e56ec6cc65c314d02fac46c7eca29045671329
Scott Olesen <ulp7@cdc.gov>
2024-12-18 09:25:39 -0500

    fixup: looping

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,26 @@
 import argparse
-import datetime as dt
 
 import nisapi
 import polars as pl
 import yaml
 
 import iup
-from iup import eval
-from iup.models import LinearIncidentUptakeModel
+import iup.eval
+import iup.models
 
 
 def run(config: dict, cache: str):
+    forecast_dates = pl.date_range(
+        config["timeframe"]["start"],
+        config["timeframe"]["end"],
+        config["timeframe"]["interval"],
+        eager=True,
+    )
+    models = [getattr(iup.models, model_name) for model_name in config["models"]]
+    assert all(issubclass(model, iup.models.UptakeModel) for model in models)
+    score_funs = [getattr(iup.eval, score_name) for score_name in config["score_funs"]]
+
+    # Preprocessing -----------------------------------------------------------
     # Get uptake data from the cache
     data = nisapi.get_nis(cache)
 
@@ -25,7 +35,7 @@ def run(config: dict, cache: str):
         for x in config["data"].values()
     ]
 
-    # Insure that the desired grouping factors are found in all data sets
+    # Ensure that the desired grouping factors are found in all data sets
     grouping_factors = config["groups"]
     assert all(g in df.columns for g in grouping_factors for df in cumulative_data)
 
@@ -38,91 +48,51 @@ def run(config: dict, cache: str):
     # List of incident data sets from the cumulative data sets
     incident_data = [x.to_incident(grouping_factors) for x in cumulative_data]
 
-    # Concatenate data sets and split into train and test subsets
-    incident_train_data = iup.IncidentUptakeData(
-        iup.IncidentUptakeData.split_train_test(
-            incident_data, config["timeframe"]["start"], "train"
-        )
-    )
+    # Forecasts ---------------------------------------------------------------
 
-    # Fit models using the training data and make projections
-    incident_model = LinearIncidentUptakeModel().fit(
-        incident_train_data, grouping_factors
-    )
+    for model in models:
+        for forecast_date in forecast_dates:
+            # Get data available as of the forecast date
+            incident_train_data = iup.IncidentUptakeData(
+                iup.IncidentUptakeData.split_train_test(
+                    incident_data, config["timeframe"]["start"], "train"
+                )
+            )
 
-    option = config["option"]
+            # Fit models using the training data and make projections
+            fit_model = model().fit(incident_train_data, grouping_factors)
 
-    if option == "projection":
-        cumulative_projections = incident_model.predict(
-            config["timeframe"]["start"],
-            config["timeframe"]["end"],
-            config["timeframe"]["interval"],
-            grouping_factors,
-        )
-        print(cumulative_projections)
-        incident_projections = cumulative_projections.to_incident(grouping_factors)
-        print(incident_projections)
-
-    elif option == "evaluation":
-        # Make projections sequentially
-        interval_str = config["timeframe"]["interval"]
-        interval_number = int(interval_str[0])
-
-        dates = pl.date_range(
-            config["timeframe"]["start"],
-            config["timeframe"]["end"] - dt.timedelta(interval_number),
-            config["timeframe"]["interval"],
-            eager=True,
-        )
-
-        scores = pl.DataFrame(
-            schema={
-                "forecast_start": pl.Date,
-                "forecast_end": pl.Date,
-                "score": pl.Float64,
-                "type": pl.String,
-            }
-        )
-
-        for date in dates:
-            # Generate cumulative predictions
-            cumulative_projections = incident_model.predict(
-                date,
+            cumulative_projections = fit_model.predict(
+                config["timeframe"]["start"],
                 config["timeframe"]["end"],
                 config["timeframe"]["interval"],
                 grouping_factors,
             )
+            # save these projections somewhere
 
-            pred = iup.PointForecast(
-                cumulative_projections.to_incident(grouping_factors)
-            )
-            test_data = iup.IncidentUptakeData(
+            incident_projections = cumulative_projections.to_incident(grouping_factors)
+            # save these projections somewhere
+
+            # Evaluation / Post-processing --------------------------------------------
+
+            incident_test_data = iup.IncidentUptakeData(
                 iup.IncidentUptakeData.split_train_test(
-                    incident_data, date, "test"
-                ).filter(pl.col("date") <= config["timeframe"]["end"])
-            )
+                    incident_data, config["timeframe"]["start"], "test"
+                )
+            ).filter(pl.col("date") <= config["timeframe"]["end"])
 
-            if config["metric"] == "mspe":
-                score = eval.score(test_data, pred, eval.mspe)
-            elif config["metric"] == "mean_bias":
-                score = eval.score(test_data, pred, eval.mean_bias)
-            elif config["metric"] == "eos_abe":
-                score = eval.score(test_data, pred, eval.eos_abe)
-            elif config["metric"] == "all":
-                score = eval.score(test_data, pred, eval.mspe)
-                score = score.concat(eval.score(test_data, pred, eval.mean_bias))
-                score = score.concat(eval.score(test_data, pred, eval.eos_abe))
-
-            score = score.with_columns(type=pl.lit(config["metric"]))
-            scores = pl.concat([scores, score], how="vertical")
-
-        print(scores)
+            for score_fun in score_funs:
+                score = eval.score(incident_test_data, incident_projections, score_fun)
+                print(f"{model=} {forecast_date=} {score_fun=} {score=}")
+                # save these scores somewhere
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("--config", help="config file")
-    p.add_argument("--cache", help="NIS cache directory")
+    p.add_argument("--config", help="config file", default="scripts/config.yaml")
+    p.add_argument(
+        "--cache", help="NIS cache directory", default=".cache/nisapi/clean/"
+    )
     args = p.parse_args()
 
     with open(args.config, "r") as f:
