dad15520418db893fa05b1faec026f4aa126865e
Fuhan Yang <ab32@cdc.gov>
2024-12-18 12:48:26 -0500

    fix test

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -39,25 +39,39 @@ def pred():
     )
 
 
-@pytest.mark.parametrize("score_fun", [eval.mspe, eval.mean_bias, eval.eos_abe])
-def test_score_df(data, pred, score_fun):
+def test_score_df(data, pred):
     """
-    Return the expected forecast start, end, and correct MSPE.
+    Return the expected forecast start, end and correct MSPE.
     """
     data = iup.IncidentUptakeData(data)
     pred = iup.PointForecast(pred)
 
-    output = eval.score(data=data, pred=pred, score_fun=score_fun)
-
-    # Validate forecast start and end dates
+    output = eval.score(data=data, pred=pred, score_fun=eval.mspe)
     assert output.item(0, "forecast_start") == date(2020, 1, 1)
     assert output.item(0, "forecast_end") == date(2020, 1, 5)
-
-    # Validate score type
+    # we're not testing the actual value, just that we get some value
     assert isinstance(output["score"][0], float)
 
-    # Expected values for different scoring functions
-    expected_scores = {eval.mspe: 0.028, eval.mean_bias: -0.6, eval.eos_abe: 0.352941}
 
-    if score_fun in expected_scores:
-        assert np.isclose(output["score"][0], expected_scores[score_fun])
+def test_mspe():
+    x = np.array([0.0, 0.1, 0.7, 0.4, 0.5])
+    y = np.array([0.0, 0.2, 1.0, 0.6, 0.5])
+    assert np.isclose(eval.mspe(x, y), 0.028)
+
+
+def test_mean_bias(data, pred):
+    """
+    Return the expected forecast start, end and correct mean bias.
+    """
+    x = pl.Series([0.0, 0.1, 0.7, 0.4, 0.5])
+    y = pl.Series([0.0, 0.2, 1.0, 0.6, 0.5])
+    assert np.isclose(eval.mean_bias(x, y), -0.6)
+
+
+def test_eos_abe(data, pred):
+    """
+    Return the expected forecast start, end and correct end-of-season error%.
+    """
+    x = pl.Series([0.0, 0.1, 0.7, 0.4, 0.5])
+    y = pl.Series([0.0, 0.2, 1.0, 0.6, 0.5])
+    assert np.isclose(eval.eos_abe(x, y), 0.352941)
