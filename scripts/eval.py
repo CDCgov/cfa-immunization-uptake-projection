855d3c3cd7a83eca9832da1c75bed059123b3fc8
Scott Olesen <ulp7@cdc.gov>
2025-01-13 13:59:54 -0500

    pre-commit compliance

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,6 +1,8 @@
-                for score_fun in score_funs:
-                    score = eval.score(
-                        incident_test_data, incident_projections, score_fun
-                    )
-                    print(f"{model=} {forecast_date=} {score_fun=} {score=}")
-                    # save these scores somewhere
+import iup.eval
+
+
+def function_name(incident_test_data, incident_projections, score_funs):
+    for score_fun in score_funs:
+        score = iup.eval.score(incident_test_data, incident_projections, score_fun)
+        # save these scores somewhere
+        print(score)
