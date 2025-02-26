1b7092a96ce6f78d016cc1ef4da1c2d25756e58f
Edward Schrom <tec0@cdc.gov>
2025-02-26 16:06:36 -0500

    Made a test for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import polars as pl
 import pytest
 
 import iup
-import iup.models
+import iup.utils
 
 
 @pytest.fixture
@@ -10,32 +10,25 @@ def frame():
     """
     Make a mock data frame to test model building.
     """
-    frame = (
-        pl.DataFrame(
-            {
-                "geography": ["USA", "PA", "USA", "PA", "USA", "PA", "USA", "PA"],
-                "time_end": [
-                    "2019-12-31",
-                    "2019-12-31",
-                    "2020-01-07",
-                    "2020-01-07",
-                    "2020-01-14",
-                    "2020-01-14",
-                    "2020-01-21",
-                    "2020-01-21",
-                ],
-                "estimate": [0.0, 0.0, 1.0, 0.1, 3.0, 0.3, 4.0, 0.4],
-                "season": "2019/2020",
-                "elapsed": [0, 0, 7, 7, 14, 14, 21, 21],
-                "interval": [None, None, 7, 7, 7, 7, 7, 7],
-            }
-        )
-        .with_columns(daily=(pl.col("estimate") / pl.col("interval")).fill_null(0))
-        .with_columns(previous=pl.col("daily").shift(1).over("geography"))
-        .with_columns(time_end=pl.col("time_end").str.strptime(pl.Date, "%Y-%m-%d"))
+    frame = pl.DataFrame(
+        {
+            "geography": ["USA", "PA", "USA", "PA", "USA", "PA", "USA", "PA"],
+            "time_end": [
+                "2019-12-31",
+                "2019-12-31",
+                "2020-01-07",
+                "2020-01-07",
+                "2020-01-14",
+                "2020-01-14",
+                "2020-01-21",
+                "2020-01-21",
+            ],
+            "estimate": [0.0, 0.0, 1.0, 0.1, 3.0, 0.3, 4.0, 0.4],
+            "season": "2019/2020",
+            "elapsed": [0, 0, 7, 7, 14, 14, 21, 21],
+            "interval": [None, None, 7, 7, 7, 7, 7, 7],
+        }
     )
-
-    frame = iup.IncidentUptakeData(frame)
 
     return frame
 
@@ -45,9 +38,7 @@ def test_extract_standards(frame):
     Make a dictionary with as many keys as provided variables,
     and two values per sub-dictionary
     """
-    output = iup.models.LinearIncidentUptakeModel.extract_standards(
-        frame, ("estimate", "elapsed")
-    )
+    output = iup.utils.extract_standards(frame, ("estimate", "elapsed"))
 
     correct = {
         "estimate": {"mean": frame["estimate"].mean(), "std": frame["estimate"].std()},
