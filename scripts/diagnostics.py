9550bddcbcb12b9edef2dcceb4caaace9c7d3778
Fuhan Yang <ab32@cdc.gov>
2025-04-08 14:13:01 -0400

    allow multiple models to diagnose

--- scripts/diagnostics.py
+++ scripts/diagnostics.py
@@ -1,6 +1,6 @@
 import argparse
 import pickle
-from typing import List
+from typing import Any, Dict, List
 
 import arviz as az
 import polars as pl
@@ -9,59 +9,86 @@ import yaml
 import iup
 import iup.diagnostics
 import iup.models
+from iup.utils import parse_name_and_date
 
 
 #### Given fit_model.mcmc, return diagnostic plot and summary ###
-def diagnostic_plot(models: List[iup.models.UptakeModel], config, output_dir):
+def diagnostic_plot(
+    models: Dict[str, iup.models.UptakeModel], config: Dict[str, Any], output_dir
+):
     """select the fitted model using model name and training end date
     and generate selected diagnostic plots"""
 
-    sel_model = select_model_to_diagnose(models, config)
+    sel_model_dicts = select_model_to_diagnose(models, config)
 
     diagnose_plot_names = config["diagnostics"]["plot"]
 
-    idata = az.from_numpyro(sel_model.mcmc)
+    for model_dict in sel_model_dicts:
+        for key, model in model_dict.items():
+            model_key = key
+            idata = az.from_numpyro(model.mcmc)
 
-    for plot_name in diagnose_plot_names:
-        plot_func = getattr(iup.diagnostics, plot_name)
-        axes = plot_func(idata)
-        fig = axes.ravel()[0].figure
-        fig.savefig(f"{output_dir}/{plot_name}.png")
+            for plot_name in diagnose_plot_names:
+                plot_func = getattr(iup.diagnostics, plot_name)
+                axes = plot_func(idata)
+                fig = axes.ravel()[0].figure
+                fig.savefig(f"{output_dir}/{model_key}_{plot_name}.png")
 
 
-def diagnostic_table(models: List[iup.models.UptakeModel], config, output_dir):
+def diagnostic_table(
+    models: Dict[str, iup.models.UptakeModel], config: Dict[str, Any], output_dir
+):
     """select the fitted model using model name and training end date
     and generate selected diagnostics: summary/posterior as parquet"""
-    sel_model = select_model_to_diagnose(models, config)
+    sel_model_dicts = select_model_to_diagnose(models, config)
 
     diagnose_table_names = config["diagnostics"]["table"]
 
-    idata = az.from_numpyro(sel_model.mcmc)
-    for table_name in diagnose_table_names:
-        table_func = getattr(iup.diagnostics, table_name)
-        output = pl.from_pandas(table_func(idata))
-        output.write_parquet(f"{output_dir}/{table_name}.parquet")
+    for model_dict in sel_model_dicts:
+        for key, model in model_dict.items():
+            model_key = key
+            idata = az.from_numpyro(model.mcmc)
+
+            for table_name in diagnose_table_names:
+                table_func = getattr(iup.diagnostics, table_name)
+                output = pl.from_pandas(table_func(idata))
+                output.write_parquet(f"{output_dir}/{model_key}_{table_name}.parquet")
 
 
 ## select the model to diagnose ##
-def select_model_to_diagnose(models: List[iup.models.UptakeModel], config):
+def select_model_to_diagnose(
+    models: Dict[str, iup.models.UptakeModel], config
+) -> List[dict]:
     """Select the model to diagnose based on the model name and the training end date"""
-    sel_models = [
-        model
-        for model in models
-        if model.__class__.__name__ == config["diagnostics"]["model"]
+    key_list = [key for key in models]
+
+    if config["diagnostics"]["forecast_date"] is None:
+        forecast_dates = pl.date_range(
+            config["forecast_timeframe"]["start"],
+            config["forecast_timeframe"]["end"],
+            config["forecast_timeframe"]["interval"],
+            eager=True,
+        )
+    elif len(config["diagnostics"]["forecast_date"]) == 1:
+        forecast_dates = config["diagnostics"]["forecast_date"]
+    else:
+        forecast_dates = pl.date_range(
+            config["diagnostics"]["forecast_date"][0],
+            config["diagnostics"]["forecast_date"][1],
+            interval=config["forecast_timeframe"]["interval"],
+            eager=True,
+        )
+
+    sel_keys = [
+        key
+        for key in key_list
+        if parse_name_and_date(key)["model_name"] in config["diagnostics"]["model"]
+        if parse_name_and_date(key)["forecast_date"] in forecast_dates
     ]
 
-    sel_model = [
-        model
-        for model in sel_models
-        if model.end_date[0, 0] == config["diagnostics"]["train_end"]
-    ]
+    sel_models = [{key: models[key]} for key in sel_keys]
 
-    assert len(sel_model) == 1, f"Expected 1 model, got {len(sel_model)}"
-    sel_model = sel_model[0]
-
-    return sel_model
+    return sel_models
 
 
 if __name__ == "__main__":
