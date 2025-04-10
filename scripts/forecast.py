1ecf80c94008bd4e2f40b2e656b89f6cf8d279f9
Edward Schrom <tec0@cdc.gov>
2025-04-10 14:53:26 -0400

    Further condensed away a loop

--- scripts/forecast.py
+++ scripts/forecast.py
@@ -2,7 +2,6 @@ import argparse
 import pickle
 from typing import Any, Dict
 
-import numpy as np
 import polars as pl
 import yaml
 
@@ -32,24 +31,16 @@ def run_all_forecasts(
         A pl.DataFrame saving predictive distribution at each time point between
         forecast start and end, at least grouped by model name, forecast start, and forecast end.
     """
+    all_forecasts = pl.DataFrame()
 
-    forecast_dates_list = [
-        parse_name_and_date(str)["forecast_date"] for str in fitted_models.keys()
-    ]
-    forecast_dates = np.array(forecast_dates_list)
-    forecast_dates = np.unique(forecast_dates)
-    forecast_dates.sort()
+    for model_details, fitted_model in fitted_models.items():
+        model_details = parse_name_and_date(model_details)
+        model_name = model_details["model_name"]
+        forecast_date = model_details["forecast_date"]
 
-    model_names = [
-        parse_name_and_date(str)["model_name"] for str in fitted_models.keys()
-    ]
-    all_forecast = pl.DataFrame()
-
-    for model_name in model_names:
         assert hasattr(iup.models, model_name), (
             f"{model_name} is not a valid model type!"
         )
-
         model_class = getattr(iup.models, model_name)
 
         augmented_data = model_class.augment_data(
@@ -60,43 +51,33 @@ def run_all_forecasts(
             config["data"]["rollouts"],
         )
 
-        for forecast_date in forecast_dates:
-            sel_key = [
-                key
-                for key in fitted_models
-                if parse_name_and_date(key)["forecast_date"] == forecast_date
-                if parse_name_and_date(key)["model_name"] == model_name
-            ]
+        test_data = iup.UptakeData.split_train_test(
+            augmented_data, forecast_date, "test"
+        )
+        if test_data.height == 0:
+            test_dates = None
+        else:
+            test_dates = test_data.select(["time_end", "season"])
 
-            model = fitted_models[sel_key[0]]
+        forecast = fitted_model.predict(
+            start_date=forecast_date,
+            end_date=config["forecast_timeframe"]["end"],
+            interval=config["forecast_timeframe"]["interval"],
+            test_dates=test_dates,
+            groups=config["data"]["groups"],
+            season_start_month=config["data"]["season_start_month"],
+            season_start_day=config["data"]["season_start_day"],
+        )
 
-            test_data = iup.UptakeData.split_train_test(
-                augmented_data, forecast_date, "test"
-            )
-            if test_data.height == 0:
-                test_dates = None
-            else:
-                test_dates = test_data.select(["time_end", "season"])
+        forecast = forecast.with_columns(
+            forecast_start=forecast_date,
+            forecast_end=config["forecast_timeframe"]["end"],
+            model=pl.lit(model_name),
+        )
 
-            forecast = model.predict(
-                start_date=forecast_date,
-                end_date=config["forecast_timeframe"]["end"],
-                interval=config["forecast_timeframe"]["interval"],
-                test_dates=test_dates,
-                groups=config["data"]["groups"],
-                season_start_month=config["data"]["season_start_month"],
-                season_start_day=config["data"]["season_start_day"],
-            )
+        all_forecasts = pl.concat([all_forecasts, forecast])
 
-            forecast = forecast.with_columns(
-                forecast_start=forecast_date,
-                forecast_end=config["forecast_timeframe"]["end"],
-                model=pl.lit(model_name),
-            )
-
-            all_forecast = pl.concat([all_forecast, forecast])
-
-    return all_forecast
+    return all_forecasts
 
 
 if __name__ == "__main__":
