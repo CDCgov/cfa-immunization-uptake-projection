d644191dcc8011cf33610a7d7fa9d685550bbe32
Scott Olesen <ulp7@cdc.gov>
2025-05-05 11:03:18 -0400

    Allow for programmable channels

--- scripts/viz.py
+++ scripts/viz.py
@@ -6,55 +6,70 @@ import streamlit as st
 def app():
     st.title("Forecasts")
 
-    # load forecast data
+    # load data
     data = load_data()
 
-    # filter to season
-    seasons = data["forecasts"]["season"].unique().to_list()
-    season = st.selectbox("Season", options=seasons)
+    # clean data
+    forecast_data = (
+        data["forecasts"]
+        .with_columns(
+            pl.col("forecast_start").dt.truncate("1d"),
+            pl.col("sample_id").cast(pl.Int32),
+        )
+        .drop("forecast_end")
+    )
+
+    # get all forecast start dates and cross them with the observed data, as
+    # if the observed data were a model
+    forecast_starts = forecast_data.select(["season", "forecast_start"]).unique()
+
+    obs_data = (
+        data["nis_raw"]
+        .with_columns(sample_id=0, model=pl.lit("observed"))
+        .join(forecast_starts, on=["season"], how="inner")
+        .select(forecast_data.columns)
+    )
+
+    data = pl.concat([forecast_data, obs_data], how="vertical")
+
+    # set up plot encodings
+    encodings = [
+        alt.X("time_end:T", title="Observation date"),
+        alt.Y("estimate:Q", title="Cumulative uptake estimate"),
+        alt.Detail("sample_id", title="Trajectory"),
+    ]
+
+    # select which data dimension to put into which plot channel
+    st.header("Plot options")
+    st.subheader("Data channels")
+    dimensions = ["season", "model", "forecast_start"]
+    for channel, default_dim in [
+        ("Color", "model"),
+        ("Column", "season"),
+        ("Row", "forecast_start"),
+    ]:
+        options = ["None"] + dimensions
+        index = options.index(default_dim)
+        dim = st.selectbox(f"{channel} by", options=options, index=index)
+
+        if dim != "None":
+            dimensions.remove(dim)
+            encodings.append(getattr(alt, channel)(dim))
+
+    # filter for specific values of the remaining dimensions
+    st.header("Data filters")
+    for dim in dimensions:
+        filter_val = st.selectbox(dim, options=data[dim].unique().sort().to_list())
+        data = data.filter(pl.col(dim) == pl.lit(filter_val))
 
     # how many trajectories to show?
     n_samples = st.number_input("Number of forecasts", value=3, min_value=1)
-
-    # filter the forecast data to a particular season, number of forecast
-    # trajectories, and forecast date
-    forecast_data = data["forecasts"].filter(
-        pl.col("season") == pl.lit(season),
+    data = data.filter(
         pl.col("sample_id").cast(pl.Int32) <= n_samples,
     )
-    forecast_dates = (
-        forecast_data["forecast_start"].dt.truncate("1d").unique().to_list()
-    )
-    forecast_date = st.select_slider("Forecast date", options=forecast_dates)
-    forecast_data = forecast_data.filter(
-        pl.col("forecast_start").dt.truncate("1d") == forecast_date
-    )
 
-    forecast_chart = (
-        alt.Chart(forecast_data)
-        .mark_line()
-        .encode(
-            x=alt.X("time_end:T", title="Observation date"),
-            y="estimate",
-            color="model",
-            detail="sample_id",
-        )
-    )
+    chart = alt.Chart(data).mark_line().encode(*encodings)
 
-    # prepare the observed data and chart
-    obs_data = data["nis_raw"].filter(pl.col("season") == pl.lit(season))
-
-    obs_chart = (
-        alt.Chart(obs_data)
-        .mark_point()
-        .encode(
-            x=alt.X("time_end:T", title="Observation date"),
-            y=alt.Y("estimate:Q", title="Cumulative uptake estimate"),
-        )
-    )
-
-    # combine and display chart
-    chart = obs_chart + forecast_chart
     st.altair_chart(chart, use_container_width=True)
 
 
