8150f61bd2182197376eef56343c1324fb677af3
Edward Schrom <tec0@cdc.gov>
2025-05-13 14:37:17 -0400

    Added more plots and observed bad forecast

--- scripts/figs_demo.py
+++ scripts/figs_demo.py
@@ -72,14 +72,66 @@ alt.Chart(state).mark_line().encode(
 )
 # %% Plot uptake for one state, with uncertainty
 one_state = state.filter(
-    (pl.col("geography") == "Kansas") & (pl.col("season") == "2020/2021")
+    (pl.col("geography") == "Kansas") & (pl.col("season") == "2023/2024")
 ).with_columns(
     estimate_hi=pl.col("estimate") + 2 * pl.col("sem"),
     estimate_lo=pl.col("estimate") - 2 * pl.col("sem"),
 )
-alt.Chart(one_state).mark_area(color="black", opacity=0.3).encode(
-    x=alt.X("elapsed:Q", title="Days since July 1"), y="estimate_lo", y2="estimate_hi"
-) + alt.Chart(one_state).mark_line(color="black").encode(
+alt.Chart(one_state).mark_errorbar(color="black").encode(
+    x=alt.X("elapsed:Q", title="Days since July 1"),
+    y=alt.Y("estimate_lo", title="Observed Uptake"),
+    y2="estimate_hi",
+) + alt.Chart(one_state).mark_point(color="black").encode(
     x=alt.X("elapsed:Q", title="Days since July 1"),
     y=alt.Y("estimate:Q", title="Observed Uptake"),
+)
+
+# %% Load forecasts for flu by state in 2023/2024, and summarize
+pred = pl.read_parquet(
+    "/home/tec0/cfa-immunization-uptake-projection/output/forecasts/tables/forecasts.parquet"
+)
+pred_summ = (
+    pred.group_by(["time_end", "geography"])
+    .agg(
+        estimate=pl.col("estimate").mean(),
+        upper=pl.col("estimate").quantile(0.975),
+        lower=pl.col("estimate").quantile(0.025),
+    )
+    .join(
+        state.select(["time_end", "geography", "estimate", "elapsed"]),
+        on=["time_end", "geography"],
+        how="left",
+    )
+    .rename({"estimate_right": "obs"})
+)
+
+# %% Plot prediction vs. data for 2023/2024
+pred_one_state = pred_summ.filter((pl.col("geography") == "Kansas"))
+(
+    alt.Chart(one_state)
+    .mark_errorbar(color="black")
+    .encode(
+        x=alt.X("elapsed:Q", title="Days since July 1"),
+        y=alt.Y("estimate_lo", title="Uptake"),
+        y2="estimate_hi",
+    )
+    + alt.Chart(one_state)
+    .mark_point(color="black")
+    .encode(
+        x=alt.X("elapsed:Q", title="Days since July 1"),
+        y=alt.Y("estimate:Q", title="Uptake"),
+    )
+    + alt.Chart(pred_one_state)
+    .mark_area(color="green", opacity=0.3)
+    .encode(
+        x=alt.X("elapsed:Q", title="Days since July 1"),
+        y=alt.Y("lower", title="Uptake"),
+        y2="upper",
+    )
+    + alt.Chart(pred_one_state)
+    .mark_line(color="green")
+    .encode(
+        x=alt.X("elapsed:Q", title="Days since July 1"),
+        y=alt.Y("estimate:Q", title="Uptake"),
+    )
 )
