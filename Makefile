8e4dc53b0a3197b36f961c748cd5f960326a76d9
Edward Schrom <tec0@cdc.gov>
2025-09-26 13:56:04 -0400

    Build Polynomial Splice Polynomial model

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-RUN_ID = lsli
+RUN_ID = psp
 TOKEN_PATH = scripts/socrata_app_token.txt
 TOKEN = $(shell cat $(TOKEN_PATH))
-CONFIG = scripts/config_lsli.yaml
+CONFIG = scripts/config_psp.yaml
 SETTINGS = output/settings/$(RUN_ID)/
 RAW_DATA = output/data/$(RUN_ID)/
 MODEL_FITS = output/fits/$(RUN_ID)/
--- iup/models.py
+++ iup/models.py
@@ -80,6 +80,450 @@ class UptakeModel(abc.ABC):
     mcmc = None
 
 
+class PSPModel(UptakeModel):
+    """
+    Subclass of UptakeModel for a mixed Polynomial Splice Polynomial model.
+    """
+
+    def __init__(self, seed: int):
+        """
+        Initialize with a seed and the model structure.
+
+        Parameters
+        seed: int
+            The random seed for stochastic elements of the model, to be split for fitting vs. predicting.
+        """
+        self.rng_key = random.key(seed)
+        self.fit_key, self.pred_key = random.split(self.rng_key, 2)
+        self.model = PSPModel._poly_splice_poly
+
+    @staticmethod
+    def _poly_splice_poly(
+        elapsed,
+        N_vax=None,
+        N_tot=None,
+        groups=None,
+        num_group_factors=0,
+        num_group_levels=[0],
+        c1_shape=1.0,
+        c1_rate=1.0,
+        c1_sig=1.0,
+        s1_shape=1.0,
+        s1_rate=1.0,
+        s1_sig=1.0,
+        a1_shape=1.0,
+        a1_rate=1.0,
+        a1_sig=1.0,
+        n1_shape=1.0,
+        n1_rate=1.0,
+        n1_sig=1.0,
+        s2_shape=1.0,
+        s2_rate=1.0,
+        s2_sig=1.0,
+        n2_shape1=1.0,
+        n2_shape2=1.0,
+        n2_sig=1.0,
+        k_shape1=1.0,
+        k_shape2=1.0,
+        k_sig=1.0,
+        d_shape=350.0,
+        d_rate=1.0,
+    ):
+        """
+        Fit a mixed Polynomial Splice Polynomial model on training data.
+
+        Parameters
+        elapsed: np.array
+            fraction of a year elapsed since the start of season at each data point
+        N_vax: np.array | None
+            number of people vaccinated at each data point
+        N_tot: np.array | None
+            number of people contacted at each data point
+        groups: np.array | None
+            numeric codes for groups: row = data point, col = grouping factor
+        num_group_factors: Int
+            number of grouping factors
+        num_group_levels: List[Int,]
+            number of unique levels of each grouping factor
+        other parameters: float
+            parameters to specify the prior distributions
+
+        Returns
+        Nothing
+
+        Details
+        Provides the model structure and priors for a Logistic Splice Linear model.
+        """
+        # Sample the overall average value for each parameter
+        c1 = numpyro.sample("c1", dist.Gamma(c1_shape, c1_rate))
+        s1 = numpyro.sample("s1", dist.Gamma(s1_shape, s1_rate))
+        a1 = numpyro.sample("a1", dist.Gamma(a1_shape, a1_rate))
+        n1 = numpyro.sample("n1", dist.Gamma(n1_shape, n1_rate))
+        s2 = numpyro.sample("s2", dist.Gamma(s2_shape, s2_rate))
+        n2 = numpyro.sample("n2", dist.Beta(n2_shape1, n2_shape2))
+        k = numpyro.sample("k", dist.Beta(k_shape1, k_shape2))
+        d = numpyro.sample("d", dist.Gamma(d_shape, d_rate))
+        # If grouping factors are given, find the group-specific deviations for each datum
+        if groups is not None:
+            c1_sigs = numpyro.sample(
+                "c1_sigs", dist.Exponential(c1_sig), sample_shape=(num_group_factors,)
+            )
+            s1_sigs = numpyro.sample(
+                "s1_sigs", dist.Exponential(s1_sig), sample_shape=(num_group_factors,)
+            )
+            a1_sigs = numpyro.sample(
+                "a1_sigs", dist.Exponential(a1_sig), sample_shape=(num_group_factors,)
+            )
+            n1_sigs = numpyro.sample(
+                "n1_sigs", dist.Exponential(n1_sig), sample_shape=(num_group_factors,)
+            )
+            s2_sigs = numpyro.sample(
+                "s2_sigs", dist.Exponential(s2_sig), sample_shape=(num_group_factors,)
+            )
+            n2_sigs = numpyro.sample(
+                "n2_sigs", dist.Exponential(n2_sig), sample_shape=(num_group_factors,)
+            )
+            k_sigs = numpyro.sample(
+                "k_sigs", dist.Exponential(k_sig), sample_shape=(num_group_factors,)
+            )
+            c1_devs = numpyro.sample(
+                "c1_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(c1_sigs, np.array(num_group_levels))
+            s1_devs = numpyro.sample(
+                "s1_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(s1_sigs, np.array(num_group_levels))
+            a1_devs = numpyro.sample(
+                "a1_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(a1_sigs, np.array(num_group_levels))
+            n1_devs = numpyro.sample(
+                "n1_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(n1_sigs, np.array(num_group_levels))
+            s2_devs = numpyro.sample(
+                "s2_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(s2_sigs, np.array(num_group_levels))
+            n2_devs = numpyro.sample(
+                "n2_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(n2_sigs, np.array(num_group_levels))
+            k_devs = k_devs = numpyro.sample(
+                "k_devs", dist.Normal(0, 1), sample_shape=(sum(num_group_levels),)
+            ) * np.repeat(k_sigs, np.array(num_group_levels))
+            c1_tot = np.sum(c1_devs[groups], axis=1) + c1
+            s1_tot = np.sum(s1_devs[groups], axis=1) + s1
+            a1_tot = np.sum(a1_devs[groups], axis=1) + a1
+            n1_tot = np.sum(n1_devs[groups], axis=1) + n1
+            s2_tot = np.sum(s2_devs[groups], axis=1) + s2
+            n2_tot = np.sum(n2_devs[groups], axis=1) + n2
+            k_tot = np.sum(k_devs[groups], axis=1) + k
+            # Calculate the determined parameters of the second polynomial
+            a2_tot = (
+                a1_tot
+                * n1_tot
+                / n2_tot
+                * ((k_tot - s1_tot) ** (n1_tot - 1))
+                / ((k_tot - s2_tot) ** (n2_tot - 1))
+            )
+            c2_tot = (
+                c1_tot
+                + a1_tot * ((k_tot - s2_tot) ** n1_tot)
+                - a1_tot
+                * n1_tot
+                / n2_tot
+                * ((k_tot - s1_tot) ** (n1_tot - 1))
+                * (k_tot - s2_tot)
+            )
+            # Calculate latent true uptake at each datum
+            mu = (c1_tot + a1_tot * ((elapsed - s1_tot) ** n1_tot)) * (elapsed <= k) + (
+                c2_tot + a2_tot * ((elapsed - s2_tot) ** n2_tot)
+            ) * (elapsed > k)
+        else:
+            # Calculate the determined parameters of the second polynomial
+            a2 = a1 * n1 / n2 * ((k - s1) ** (n1 - 1)) / ((k - s2) ** (n2 - 1))
+            c2 = (
+                c1
+                + a1 * ((k - s2) ** n1)
+                - a1 * n1 / n2 * ((k - s1) ** (n1 - 1)) * (k - s2)
+            )
+            # Calculate latent true uptake at each datum
+            mu = (c1 + a1 * ((elapsed - s1) ** n1)) * (elapsed <= k) + (
+                c2 + a2 * ((elapsed - s2) ** n2)
+            ) * (elapsed > k)
+        # Calculate the shape parameters for the beta-binomial likelihood
+        S1 = mu * d
+        S2 = (1 - mu) * d
+        numpyro.sample("obs", dist.BetaBinomial(S1, S2, N_tot), obs=N_vax)
+
+    @staticmethod
+    def augment_data(
+        data: CumulativeUptakeData,
+        season_start_month: int,
+        season_start_day: int,
+    ) -> CumulativeUptakeData:
+        """
+        Format preprocessed data for fitting a Polynomial Splice Polynomial model.
+
+        Parameters:
+        data: CumulativeUptakeData
+            training data for fitting a Polynomial Splice Polynomial model
+         season_start_month: int
+            first month of the overwinter disease season
+        season_start_day: int
+            first day of the first month of the overwinter disease season
+
+        Returns:
+            Cumulative uptake data ready for fitting a Polynomial Splice Polynomial model.
+
+        Details
+        The following steps are required to prepare preprocessed data:
+        - Add an extra columns for time elapsed since start-of-season
+        - Rescale this time elapsed to a proportion of the year
+        """
+        data = CumulativeUptakeData(
+            data.with_columns(
+                elapsed=iup.utils.date_to_elapsed(
+                    pl.col("time_end"),
+                    season_start_month,
+                    season_start_day,
+                )
+                / 365
+            )
+        )
+
+        return data
+
+    def fit(
+        self,
+        data: CumulativeUptakeData,
+        groups: List[str,] | None,
+        params: dict,
+        mcmc: dict,
+    ) -> Self:
+        """
+        Fit a mixed Polynomial Splice Polynomial model on training data.
+
+        Parameters
+        data: CumulativeUptakeData
+            training data on which to fit the model
+        group_cols: (str,) | None
+            name(s) of the columns for the grouping factors
+        params: dict
+            parameter names and values to specify prior distributions
+        mcmc: dict
+            control parameters for mcmc fitting
+
+        Returns
+        PSPModel
+            model object with grouping factor combinaions
+            and the model fit all stored as attributes
+
+        Details
+        If grouping factors are specified, a hierarchical model will be built with
+        group-specific parameters for the logistic maximum and linear slope,
+        drawn from a shared distribution. Other parameters are non-hierarchical.
+        """
+        self.group_combos = extract_group_combos(data, groups)
+
+        # Tranform the levels of the grouping factors into numeric codes
+        if groups is not None:
+            self.num_group_factors = len(groups)
+            self.num_group_levels = iup.utils.count_unique_values(self.group_combos)
+            self.value_to_index = iup.utils.map_value_to_index(data.select(groups))
+            group_codes = iup.utils.value_to_index(
+                data.select(groups), self.value_to_index, self.num_group_levels
+            )
+        else:
+            group_codes = None
+            self.num_group_factors = 0
+            self.num_group_levels = [0]
+            self.value_to_index = None
+
+        # Prepare the data to be fed to the model. Must be numpy arrays.
+        elapsed = data["elapsed"].to_numpy()
+        N_vax = data["N_vax"].to_numpy()
+        N_tot = data["N_tot"].to_numpy()
+
+        self.kernel = NUTS(self.model, init_strategy=init_to_sample)
+        self.mcmc = MCMC(
+            self.kernel,
+            num_warmup=mcmc["num_warmup"],
+            num_samples=mcmc["num_samples"],
+            num_chains=mcmc["num_chains"],
+        )
+
+        self.mcmc.run(
+            self.fit_key,
+            elapsed=elapsed,
+            N_vax=N_vax,
+            N_tot=N_tot,
+            groups=group_codes,
+            num_group_factors=self.num_group_factors,
+            num_group_levels=self.num_group_levels,
+            c1_shape=params["c1_shape"],
+            c1_rate=params["c1_rate"],
+            c1_sig=params["c1_sig"],
+            s1_shape=params["s1_shape"],
+            s1_rate=params["s1_rate"],
+            s1_sig=params["s1_sig"],
+            a1_shape=params["a1_shape"],
+            a1_rate=params["a1_rate"],
+            a1_sig=params["a1_sig"],
+            n1_shape=params["n1_shape"],
+            n1_rate=params["n1_rate"],
+            n1_sig=params["n1_sig"],
+            s2_shape=params["s2_shape"],
+            s2_rate=params["s2_rate"],
+            s2_sig=params["s2_sig"],
+            n2_shape1=params["n2_shape1"],
+            n2_shape2=params["n2_shape2"],
+            n2_sig=params["n2_sig"],
+            k_shape1=params["k_shape1"],
+            k_shape2=params["k_shape2"],
+            k_sig=params["k_sig"],
+            d_shape=params["d_shape"],
+            d_rate=params["d_rate"],
+        )
+
+        print(self.mcmc.print_summary())
+
+        return self
+
+    @staticmethod
+    def augment_scaffold(
+        scaffold: pl.DataFrame, season_start_month: int, season_start_day: int
+    ) -> pl.DataFrame:
+        """
+        Add columns to a scaffold of dates for forecasting from a Polynomial Splice Polynomial model.
+
+        Parameters:
+        scaffold: pl.DataFrame
+            scaffold of dates for forecasting
+        season_start_month: int
+            first month of the overwinter disease season
+        season_start_day: int
+            first day of the first month of the overwinter disease season
+
+        Returns:
+            Scaffold with extra columns required by the Polynomial Splice Polynomial model.
+
+        Details
+        An extra column is added for the time elapsed since the season start.
+        Predictions are made as if 10,000 individuals are sampled.
+        """
+        scaffold = scaffold.with_columns(
+            elapsed=iup.utils.date_to_elapsed(
+                pl.col("time_end"), season_start_month, season_start_day
+            )
+            / 365,
+            N_tot=pl.lit(10000),
+        ).drop("estimate")
+
+        return scaffold
+
+    def predict(
+        self,
+        start_date: dt.date,
+        end_date: dt.date,
+        interval: str,
+        test_dates: pl.DataFrame | None,
+        groups: List[str,] | None,
+        season_start_month: int,
+        season_start_day: int,
+    ) -> pl.DataFrame:
+        """
+        Make projections from a fit Polynomial Splice Polynomial model.
+
+        Parameters
+        start_date: dt.date
+            the date on which projections should begin
+        end_date: dt.date
+            the date on which projections should end
+        interval: str
+            the time interval between projection dates,
+            following timedelta convention (e.g. '7d' = seven days)
+        test_dates: pl.DataFrame | None
+            exact target dates to use, when test data exists
+        groups: (str,) | None
+            name(s) of the columns for the grouping factors
+        season_start_month: int
+            first month of the overwinter disease season
+        season_start_day: int
+            first day of the first month of the overwinter disease season
+
+        Returns
+        LSLModel
+            the model with incident and cumulative projections as attributes
+
+        Details
+        A data frame is set up to house the projections over the
+        desired time window with the desired intervals.
+
+        Forecasts are the made for each date in this scaffold.
+        """
+        scaffold = build_scaffold(
+            start_date,
+            end_date,
+            interval,
+            test_dates,
+            self.group_combos,
+            season_start_month,
+            season_start_day,
+        )
+
+        scaffold = PSPModel.augment_scaffold(
+            scaffold, season_start_month, season_start_day
+        )
+
+        predictive = Predictive(self.model, self.mcmc.get_samples())
+
+        if groups is not None:
+            # Make a numpy array of numeric codes for grouping factor levels
+            # that matches the same codes used when fitting the model
+            group_codes = iup.utils.value_to_index(
+                scaffold.select(groups), self.value_to_index, self.num_group_levels
+            )
+
+            # Make a prediction-machine from the fit model
+            predictions = np.array(
+                predictive(
+                    self.pred_key,
+                    elapsed=scaffold["elapsed"].to_numpy(),
+                    N_tot=scaffold["N_tot"].to_numpy(),
+                    groups=group_codes,
+                    num_group_factors=self.num_group_factors,
+                    num_group_levels=self.num_group_levels,
+                )["obs"]
+            ).transpose()
+        else:
+            predictions = np.array(
+                predictive(self.pred_key, elapsed=scaffold["elapsed"].to_numpy())["obs"]
+            ).transpose()
+
+        pred = pl.concat(
+            [
+                scaffold,
+                pl.DataFrame(
+                    predictions,
+                    schema=[f"{i + 1}" for i in range(predictions.shape[1])],
+                ),
+            ],
+            how="horizontal",
+        )
+
+        pred = (
+            pred.unpivot(
+                index=scaffold.columns,
+                variable_name="sample_id",
+                value_name="estimate",
+            )
+            .with_columns(
+                sample_id=pl.col("sample_id"),
+                estimate=pl.col("estimate") / pl.col("N_tot"),
+            )
+            .drop(["elapsed", "N_tot"])
+        )
+
+        return SampleForecast(pred)
+
+
 class LSLIModel(UptakeModel):
     """
     Subclass of UptakeModel for a mixed Logistic Splice Linear Intercept model.
@@ -183,7 +627,7 @@ class LSLIModel(UptakeModel):
             a_tot = np.sum(a_devs[groups], axis=1) + a
             k_tot = np.sum(k_devs[groups], axis=1) + k
             n_tot = np.sum(n_devs[groups], axis=1) + n
-            c_tot = np.exp(np.sum(c_devs[groups], axis=1) + c)
+            c_tot = jnp.exp(np.sum(c_devs[groups], axis=1) + c)
             # Calculate slope and intercept of the linear
             m_tot = ((a_tot - c_tot) * n_tot * jnp.exp(0 - n_tot * (k_tot - h))) / (
                 (1 + jnp.exp(0 - n_tot * (k_tot - h))) ** 2
@@ -422,7 +866,7 @@ class LSLIModel(UptakeModel):
             season_start_day,
         )
 
-        scaffold = LSLModel.augment_scaffold(
+        scaffold = LSLIModel.augment_scaffold(
             scaffold, season_start_month, season_start_day
         )
 
