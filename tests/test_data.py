a3e67741f67a03a69600ebc484745179716f7b44
Scott Olesen <ulp7@cdc.gov>
2026-01-28 16:56:21 -0500

    fixup: test about sample ID type

--- tests/test_data.py
+++ tests/test_data.py
@@ -127,6 +127,6 @@ def test_quantile_forecast_validation():
 def test_sample_forecast_validation():
     iup.SampleForecast(
         pl.DataFrame(
-            {"time_end": [dt.date(2020, 1, 1)], "estimate": [0.0], "sample_id": "0"}
-        ).with_columns(pl.col("sample_id"))
+            {"time_end": [dt.date(2020, 1, 1)], "estimate": [0.0], "sample_id": 0}
+        ).with_columns(pl.col("sample_id").cast(pl.UInt64))
     )
