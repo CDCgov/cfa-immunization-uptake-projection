c9def62bb26ca4cffbd32befa4fd0f62012716de
Fuhan Yang <ab32@cdc.gov>
2026-02-16 14:30:34 -0500

    hierarchical gam draft

--- scripts/gam/gam_scipy.py
+++ scripts/gam/gam_scipy.py
@@ -1,6 +1,6 @@
-import altair as alt
+from typing import List
+
 import jax.numpy as jnp
-import numpy as np
 import numpyro
 import numpyro.distributions as dist
 import polars as pl
@@ -23,7 +23,7 @@ nis = (
 ).with_columns(elapsed=(pl.col("time_end") - pl.col("roll_out")).dt.total_days())
 
 
-def get_spline(x: pl.Series, y: pl.Series, p: int = 2, num_internal_knots=1) -> BSpline:
+def get_spline(x: ArrayLike, y: ArrayLike, p: int = 2, num_internal_knots=1) -> BSpline:
     """
     Get BSpline object given predictor xx-coordinates, response y, order of degree,
     and the number of internal knots.
@@ -31,9 +31,9 @@ def get_spline(x: pl.Series, y: pl.Series, p: int = 2, num_internal_knots=1) -> 
     the internal knots are evenly spaced between the boundary knots.
 
     Args:
-    x: pl.Series
+    x: ArrayLike
         the value of predictor x, must be sorted and unique
-    y: pl.Series
+    y: ArrayLike
         the value of response variable yS
     p: int
         the degree of spline
@@ -44,12 +44,6 @@ def get_spline(x: pl.Series, y: pl.Series, p: int = 2, num_internal_knots=1) -> 
     BSpline object
 
     """
-    # data
-    x = jnp.asarray(x)
-    y = jnp.asarray(y)
-
-    assert jnp.all(jnp.diff(x) >= 0), "x must not be descending."
-    assert jnp.array_equal(x, jnp.unique(x)), "Each element in x must be unique."
 
     # internal knots
     interior_knots = jnp.linspace(x.min(), x.max(), num_internal_knots + 2)[1:-1]
@@ -65,7 +59,7 @@ def get_spline(x: pl.Series, y: pl.Series, p: int = 2, num_internal_knots=1) -> 
     return bs
 
 
-def get_design_matrix(bs: BSpline, t: ArrayLike, x: pl.Series, p: int = 2) -> ArrayLike:
+def get_design_matrix(bs: BSpline, t: ArrayLike, x: ArrayLike, p: int = 2) -> ArrayLike:
     """
     Get design matrix. Each element in design matrix is evaluation of basis function at x_i data point:
     X_ij = B_j(x_i).
@@ -75,22 +69,20 @@ def get_design_matrix(bs: BSpline, t: ArrayLike, x: pl.Series, p: int = 2) -> Ar
         the BSpline object created based x and y
     t: ArrayLike
         the vector of knots
-    x: pl.Series
+    x: ArrayLike
         the values of predictor x
 
     Return:
     design matrix: ArrayLike
 
     """
-
-    x = jnp.asarray(x)
     Xobj = bs.design_matrix(x, t, p)
     X = jnp.array(Xobj.toarray())
 
     return X
 
 
-def get_penalty_matrix(t: ArrayLike, x: pl.Series, p: int) -> ArrayLike:
+def get_penalty_matrix(t: ArrayLike, x: ArrayLike, p: int = 2) -> ArrayLike:
     """
     Get penalty matrix for spline functions, penalizing the smoothness.
 
@@ -102,7 +94,7 @@ def get_penalty_matrix(t: ArrayLike, x: pl.Series, p: int) -> ArrayLike:
     Args:
     t: ArrayLike
         Knot vector for spline function
-    x: pl.Series
+    x: ArrayLike
         the data point to evaluate penalty
     p: int
         Degree of the spline
@@ -122,9 +114,8 @@ def get_penalty_matrix(t: ArrayLike, x: pl.Series, p: int) -> ArrayLike:
             # Create basis functions
             c_i = jnp.zeros(n_basis)
             c_i = c_i.at[i].set(1.0)
-            # c_i[i] = 1.0
+
             c_j = jnp.zeros(n_basis)
-            # c_j[j] = 1.0
             c_j = c_j.at[j].set(1.0)
 
             # Create B-spline objects
@@ -136,7 +127,6 @@ def get_penalty_matrix(t: ArrayLike, x: pl.Series, p: int) -> ArrayLike:
             spline_j_dd = spline_j.derivative(p - 1)
 
             # manual integration
-            # S[i,j] = sum(spline_i_dd(x)*spline_j_dd(x))
             S = S.at[i, j].set(sum(spline_i_dd(x) * spline_j_dd(x)))
 
     return S
@@ -144,94 +134,127 @@ def get_penalty_matrix(t: ArrayLike, x: pl.Series, p: int) -> ArrayLike:
 
 # numpyro model
 def gam(
-    S: ArrayLike,
-    X: ArrayLike,
-    estimate: ArrayLike,
+    data: pl.DataFrame,
+    p: int = 2,
+    num_internal_knots: int = 1,
     sigma_z_rate=40,
-    groups=None,
+    groups=["season", "geography"],
     num_group_factors=0,
     num_group_levels: ArrayLike = jnp.array([0]),
     lam_shape=1.0,
     lam_rate=1.0,
     sigma_rate=40.0,
-    beta0_mean=0.0,
-    beta0_sd=1.0,
 ):
-    n, p = X.shape
-
     # Priors
     lam = numpyro.sample("lam", dist.Gamma(lam_shape, lam_rate))
     sigma = numpyro.sample("sigma", dist.Exponential(sigma_rate))
-    # beta0 = numpyro.sample("beta0", dist.Normal(beta0_mean, beta0_sd))
-
-    # Penalized precision matrix, add 1e-6 to make sure stability
-    precision = (lam * S) + 1e-6 * jnp.eye(p)
 
     if groups is None:
-        # Sample coefficients with constraint
+        # Get the design matrix
+        y = jnp.asarray(data["estimate"])
+        x = jnp.asarray(data["elapsed"])
+
+        bs = get_spline(x, y, p, num_internal_knots)
+
+        X = get_design_matrix(bs, bs.t, x, p)
+        S = get_penalty_matrix(bs.t, x, p)
+
+        # Penalized precision matrix, add 1e-6 to make sure stability
+        n, k = X.shape
+        precision = (lam * S) + 1e-6 * jnp.eye(k)
+
+        # Sample coefficients with constraint, this is mean beta vector across all groups
         beta = numpyro.sample(
             "beta",
-            dist.MultivariateNormal(jnp.zeros(p), sigma * jnp.linalg.inv(precision)),
+            dist.MultivariateNormal(jnp.zeros(k), sigma * jnp.linalg.inv(precision)),
         )
 
         # Compute mean
         mu = X @ beta
+        # Likelihood
+        numpyro.sample("obs", dist.Normal(mu, sigma), obs=y)
 
     else:
+        data = index_combo(data, groups)
+
         sigma_z = numpyro.sample(
             "sigma_z", dist.Exponential(sigma_z_rate), sample_shape=(num_group_factors,)
         )
 
-        z = numpyro.sample(
-            "z",
-            dist.MultivariateNormal(0, jnp.eye(p)),
-            sample_shape=(sum(num_group_levels),),
-        )
+        mu_all = jnp.array([])
+        y_all = jnp.asarray(data["estimate"])
 
-        dev = (
-            z * jnp.repeat(sigma_z, num_group_levels)[:, None]
-        )  # broadcast to allowing multiplying with z
+        for idx in data["group_combo_idx"].unique():
+            single_subset = data.filter(pl.col("group_combo_idx") == idx)
 
-        beta_all = beta + jnp.sum(dev, axis=0)
+            x = jnp.asarray(single_subset["elapsed"])
 
-        mu = X @ beta_all
+            y = jnp.asarray(single_subset["estimate"])
+            bs = get_spline(x, y)
+            X = get_design_matrix(bs, bs.t, x)
 
-    # Likelihood
-    numpyro.sample("obs", dist.Normal(mu, sigma), obs=estimate)
+            S = get_penalty_matrix(bs.t, x)
+
+            n, k = X.shape
+
+            z = numpyro.sample(
+                f"z_{idx}",
+                dist.MultivariateNormal(0, jnp.eye(k)),
+                sample_shape=(sum(num_group_levels),),
+            )
+
+            dev = (
+                z * jnp.repeat(sigma_z, num_group_levels)[:, None]
+            )  # broadcast to allowing multiplying with z
+
+            # Penalized precision matrix, add 1e-6 to make sure stability
+            precision = (lam * S) + 1e-6 * jnp.eye(k)
+
+            # Sample coefficients with constraint, this is mean beta vector across all groups
+            beta = numpyro.sample(
+                f"beta_{idx}",
+                dist.MultivariateNormal(
+                    jnp.zeros(k), sigma * jnp.linalg.inv(precision)
+                ),
+            )
+
+            beta_all = beta + jnp.sum(dev, axis=0)
+            mu = X @ beta_all
+
+            mu_all = jnp.concat([mu_all, mu])
+
+        # Likelihood
+        numpyro.sample("obs", dist.Normal(mu_all, sigma), obs=y_all)
 
 
-# compare posterior mean with data #
-def posterior_mean(mcmc: MCMC, X) -> ArrayLike:
-    samples = mcmc.get_samples()
-    beta_sample = samples["beta"]
-    beta_mean = jnp.mean(beta_sample, axis=0)
-    # beta0_mean = jnp.mean(samples["beta0"])
+def index_combo(data: pl.DataFrame, groups: List):
+    data = data.with_columns(
+        group_combo=pl.concat_str([pl.col(g) for g in groups], separator="_")
+    )
+    unique_values = data["group_combo"].unique().sort().to_list()
+    indices = list(range(len(unique_values)))
 
-    mu = X @ beta_mean
+    replace_map = {value: index for value, index in zip(unique_values, indices)}
 
-    return mu
+    data = data.with_columns(
+        group_combo_idx=pl.col("group_combo").replace_strict(replace_map)
+    ).drop("group_combo")
+
+    return data
 
 
 if __name__ == "__main__":
     ## model fitting ##
 
-    nis = nis.sort("elapsed")
-
-    x = nis["elapsed"]
-    y = nis["estimate"]
-
-    p = 2
-    num_internal_knots = 1
-    bs = get_spline(x, y, p, num_internal_knots)
-
-    X = get_design_matrix(bs, bs.t, x, p)
-
-    S = get_penalty_matrix(bs.t, x, p)
-
-    estimate = jnp.array(nis["estimate"])
-
     rng_key = random.PRNGKey(0)
     rng_key, rng_key_ = random.split(rng_key)
+
+    # use a vignette to test model #
+    data = pl.read_parquet("data/raw.parquet")
+    data = data.filter(
+        pl.col("season").is_in(["2020/2021", "2019/2020"]),
+        pl.col("geography").is_in(["Alabama", "Alaska"]),
+    )
 
     kernel = NUTS(gam)
     mcmc_par = {"num_warmup": 1000, "num_samples": 2000, "num_chains": 4}
@@ -241,20 +264,18 @@ if __name__ == "__main__":
         num_samples=mcmc_par["num_samples"],
         num_chains=mcmc_par["num_chains"],
     )
-    mcmc.run(rng_key_, S=S, X=X, estimate=estimate)
 
-    ### model output ###
+    groups = ["season", "geography"]
+    num_group_levels = jnp.array([2, 2])
+
+    mcmc.run(
+        rng_key_,
+        p=2,
+        groups=groups,
+        num_group_factors=len(groups),
+        num_group_levels=num_group_levels,
+        data=data,
+    )
 
     # model summary #
     mcmc.print_summary()
-
-    # compare posterior mean with data #
-    mu = posterior_mean(mcmc, X)
-    df = pl.DataFrame({"data": nis["estimate"], "date": nis["time_end"]})
-    df = df.with_columns(mu=np.asarray(mu))
-
-    charts = alt.Chart(df).mark_point().encode(x="date", y="data") + alt.Chart(
-        df
-    ).mark_line(color="red").encode(x="date", y="mu")
-
-    charts.save("new_gam_fit_10_11.png")
