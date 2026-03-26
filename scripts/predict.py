512524284d702272d0043ce5075f7350d5be98c9
Fuhan Yang <ab32@cdc.gov>
2026-03-26 11:55:06 -0400

    refactor predict script

--- scripts/predict.py
+++ scripts/predict.py
@@ -3,22 +3,33 @@ import pickle
 from pathlib import Path
 
 import polars as pl
+import yaml
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--fits", required=True)
+    p.add_argument("--config", required=True)
     p.add_argument("--output", help="forecasts parquet", required=True)
     args = p.parse_args()
 
     with open(args.fits, "rb") as f:
         models_dict = pickle.load(f)
 
-    forecasts = pl.concat(
-        [
-            model.predict().with_columns(model=pl.lit(model.__class__.__name__))
-            for model in models_dict.values()
-        ]
-    )
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+
+    forecasts = []
+    for (k1, k2), v in models_dict.items():
+        alpha = next(
+            model["pred_params"]["alpha"]
+            for model in config["models"]
+            if model["name"] == k1
+        )
+
+        forecast = v.predict(alpha).with_columns(model=pl.lit(k1))
+        forecasts.append(forecast)
+
+    forecasts = pl.concat(forecasts)
 
     Path(args.output).parent.mkdir(parents=True, exist_ok=True)
     forecasts.write_parquet(args.output)
